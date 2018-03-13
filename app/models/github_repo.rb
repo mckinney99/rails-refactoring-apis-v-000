56c49aa23cfeaa2baa4ebd223eb7795675bc0996
Eric McKinney <mckinney99@gmail.com>
2018-03-13 20:34:52 +0000

    automatically backed up by learn

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -8,7 +8,7 @@ class GithubRepo
     end
 
     def authenticate!(client_id, client_secret, code)
-      response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
+      response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT_ID"], client_secret: ENV["GITHUB__CLIENT_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
       access_hash = JSON.parse(response.body)
       session[:token] = access_hash["access_token"]
 
